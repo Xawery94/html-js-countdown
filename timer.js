8231cdb725d6c0a60f11a4687b814a14adcc6725
KJanik <k.janik@cctechnology.pl>
2019-11-04 21:07:10 +0100

    Add background to timer

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 function makeTimer() {
 
-    //    var endTime = new Date("11 November 2019 15:57:15 GMT+01:00");
+    //    var endTime = new Date("04 November 2019 21:00:35 GMT+01:00");
     var endTime = new Date("09 Decemver 2019 00:00:00 GMT+01:00");
     endTime = (Date.parse(endTime) / 1000);
 
@@ -24,14 +24,17 @@ function makeTimer() {
         seconds = "0" + seconds;
     }
 
+    //20 days left
     if (timeLeft < 1728000) {
         $("body").css("background-image", "url(assets/home.jpg)");
     }
 
+    //10 days left
     if (timeLeft < 864000) {
         $("body").css("background-image", "url(assets/time.png)");
     }
 
+    //on time, change text
     if (timeLeft == 0 || timeLeft < 0) {
         $("#timer").css("display", "none");
         $("#title").css("display", "none");
