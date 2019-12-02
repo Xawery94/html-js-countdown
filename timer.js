17525fdd82d25009e5b78e894a26e5a8b05ba4a9
KJanik <k.janik@cctechnology.pl>
2019-12-02 15:17:57 +0100

    Fix firefox issue and css style

--- timer.js
+++ timer.js
@@ -1,7 +1,7 @@
 function makeTimer() {
 
     //    var endTime = new Date("04 November 2019 21:00:35 GMT+01:00");
-    var endTime = new Date("09 Decemver 2019 00:00:00 GMT+01:00");
+    var endTime = new Date("09 December 2019 00:00:00 GMT+01:00");
     endTime = (Date.parse(endTime) / 1000);
 
     var now = new Date();
